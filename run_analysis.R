b0d77dfbc4a6cb01ccbf0299a1b667bf7e9e300b
jbreiteneicher <janos.breiteneicher@nokia.com>
2022-01-09 19:59:04 +0100

    Sunday9th

---
+++ run_analysis.R
@@ -0,0 +1,11 @@
+### Load Data
+x_test <- read.csv("..\\UCI HAR Dataset\\test\\X_test.txt")
+y_test <- read.csv("..\\UCI HAR Dataset\\test\\Y_test.txt")
+x_train <- read.csv("..\\UCI HAR Dataset\\train\\X_train.txt")
+y_train <- read.csv("..\\UCI HAR Dataset\\train\\Y_train.txt")
+
+
+
+
+### write output
+write.table(result_df, row.name=FALSE)
