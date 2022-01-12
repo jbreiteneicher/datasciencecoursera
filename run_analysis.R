9ab28e3ea26e1d5ee5b6a523e667b3c7ba5b6881
jbreiteneicher <janos.breiteneicher@nokia.com>
2022-01-12 21:17:38 +0100

    xxx

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,8 @@ y_test <- read.csv("..\\UCI HAR Dataset\\test\\Y_test.txt")
 x_train <- read.csv("..\\UCI HAR Dataset\\train\\X_train.txt")
 y_train <- read.csv("..\\UCI HAR Dataset\\train\\Y_train.txt")
 
-
+result_df <- x_test
 
 ### write output
-write.table(result_df, row.name=FALSE)
+write.table(result_df, file="JB_result.txt", row.name=FALSE)
+
