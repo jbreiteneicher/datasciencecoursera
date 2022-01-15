f0aebbeb470bfdd84b78c5257b98256b3bd88033
jbreiteneicher <janos.breiteneicher@nokia.com>
2022-01-15 20:16:52 +0100

    Saturday

--- CodeBook.md
+++ CodeBook.md
@@ -1,5 +1,5 @@
 ---
-title: "Code Book Coursera Getting and Cleaning Data"
+title: "Code Book - Coursera Getting and Cleaning Data"
 author: "J.Breiteneicher" 
 date: "January 2022" 
 ---
@@ -11,6 +11,8 @@ The data is provided by UCI in a zip file. A full description is available at th
 > The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data.
 
 The data for the project : <https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip>
+
+After the download the files are stored in the folder ..\\UCI HAR Dataset
 
 ### Output - Tidy Result File - JB_results.txt
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,33 @@
-### Load Data
-x_test <- read.csv("..\\UCI HAR Dataset\\test\\X_test.txt")
-y_test <- read.csv("..\\UCI HAR Dataset\\test\\Y_test.txt")
-x_train <- read.csv("..\\UCI HAR Dataset\\train\\X_train.txt")
-y_train <- read.csv("..\\UCI HAR Dataset\\train\\Y_train.txt")
+library(dplyr)
 
-result_df <- x_test
+### Load Data
+x_test <- read.table("..\\UCI HAR Dataset\\test\\X_test.txt")
+y_test <- read.table("..\\UCI HAR Dataset\\test\\Y_test.txt")
+x_train <- read.table("..\\UCI HAR Dataset\\train\\X_train.txt")
+y_train <- read.table("..\\UCI HAR Dataset\\train\\Y_train.txt")
+subject_test <- read.table("..\\UCI HAR Dataset\\test\\subject_test.txt")
+subject_train <- read.table("..\\UCI HAR Dataset\\train\\subject_train.txt")
+
+### combine train and test data
+x_data <- rbind(x_train, x_test)
+y_data <- rbind(y_train, y_test)
+subject_data <- rbind(subject_train, subject_test)
+
+### Step 2
+features <- read.table("..\\UCI HAR Dataset\\features.txt")
+mean_features <- grep("-(mean|std)\\(\\)", features[, 2])
+
+# subset the desired columns
+x_data <- x_data[, mean_features]
+
+names(x_data) <- features[mean_features, 2]
+
+### Step 3
+activities <- read.table("..\\UCI HAR Dataset\\activity_labels.txt")
+
+
+## Step 5
+result_df <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
 ### write output
 write.table(result_df, file="JB_result.txt", row.name=FALSE)
