95fba9b4be8a7ec2c8f6bb0d45b0c4044f75bd7c
jbreiteneicher <janos.breiteneicher@nokia.com>
2022-01-23 13:46:10 +0100

    sunday

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-library(dplyr)
+library(plyr)
 
 ### Load Data
 x_test <- read.table("..\\UCI HAR Dataset\\test\\X_test.txt")
@@ -24,10 +24,25 @@ names(x_data) <- features[mean_features, 2]
 
 ### Step 3
 activities <- read.table("..\\UCI HAR Dataset\\activity_labels.txt")
+y_data[, 1] <- activities[y_data[, 1], 2]
+names(y_data) <- "activity"
 
+
+# correct column name
+names(y_data) <- "activity"
+
+# Step 4
+# Appropriately label the data set with descriptive variable names
+###############################################################################
+
+# correct column name
+names(subject_data) <- "subject"
+
+# bind all the data in a single data set
+all_data <- cbind(x_data, y_data, subject_data)
 
 ## Step 5
-result_df <- saply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+result_df <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
 ### write output
 write.table(result_df, file="JB_result.txt", row.name=FALSE)
